ce0c2f19fac9995a738f6b9357f9c98a46ede12c
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-09-03 15:39:32 +0300

    Validation for birth date (>=16).

--- js/index.js
+++ js/index.js
@@ -44,51 +44,58 @@ function Add_Employee(){
             if(email==""){
                 alert("Completati email-ul");
             }else{
-                arrayEmployee[2] = email;
-                if(sex=="-"){
-                    alert("Alegeti sex-ul");
-                }else{
-                    var sexValue;
-                    if(sex == "f"){
-                        sexValue="feminin";
+                    arrayEmployee[2] = email;
+                    if(sex=="-"){
+                       alert("Alegeti sex-ul");
                     }else{
-                        sexValue="masculin";
-                    }
-                    arrayEmployee[3] = sexValue;
-                    if(data_nasterii==""){
-                        alert("Alegeti data nasterii");
-                    }else{
-                        arrayEmployee[4] = data_nasterii;
-                        if(poza_angajat==""){
-                            alert("Alegeti poza");
+                       var sexValue;
+                       if(sex == "f"){
+                          sexValue="feminin";
                         }else{
-                            arrayEmployee[5] = poza_angajat.split("\\")[2];
-                                localStorage.setItem(`${nume}`, arrayEmployee); 
-                                var archive = [];
-                                var table = document.getElementById("tabelAngajati");
-                                for (var i = 0; i<localStorage.length; i++) {
-                                    archive[i] = localStorage.getItem(localStorage.key(i));
-                                    var arrayEmployee =  archive[i].split(",");
-                                    var row= table.insertRow(j);
-                                    var cellNume = row.insertCell();
-                                    cellNume.innerHTML = arrayEmployee[0];
-                                    var cellPrenume = row.insertCell();
-                                    cellPrenume.innerHTML = arrayEmployee[1];
-                                    var cellEmail = row.insertCell();
-                                    cellEmail.innerHTML = arrayEmployee[2];
-                                    var cellSex = row.insertCell();
-                                    cellSex.innerHTML = arrayEmployee[3];
-                                    var cellDataNasterii = row.insertCell();
-                                    cellDataNasterii.innerHTML = arrayEmployee[4];
-                                    var cellPozaAngajat = row.insertCell();
-                                    cellPozaAngajat.innerHTML = `<img src="./images/${arrayEmployee[5]}" />`;
-                                    var cellDelete = row.insertCell();
-                                    cellDelete.innerHTML = '<img src="./images/recycle-bin.png" class="recycleBin"/>';
-                                    j++;
+                          sexValue="masculin";
+                        }
+                        arrayEmployee[3] = sexValue;
+                        if(data_nasterii==""){
+                          alert("Alegeti data nasterii");
+                        }else{
+                           var birthDate = new Date(data_nasterii);
+                           var month_diff = Date.now() - birthDate.getTime();  
+                           var age_dt = new Date(month_diff);   
+                           var year = age_dt.getUTCFullYear();  
+                           var age = Math.abs(year - 1970); 
+                           if(age>=16){
+                              arrayEmployee[4] = data_nasterii;
+                              if(poza_angajat==""){
+                                alert("Alegeti poza");
+                                }else{
+                                    arrayEmployee[5] = poza_angajat.split("\\")[2];
+                                    localStorage.setItem(`${nume}`, arrayEmployee); 
+                                    var archive;
+                                    var table = document.getElementById("tabelAngajati");
+                                        archive= localStorage.getItem(localStorage.getItem(`${nume}`));
+                                        var arrayEmployee =  archive.split(",");
+                                        var row= table.insertRow(localStorage.length+1);
+                                        var cellNume = row.insertCell();
+                                        cellNume.innerHTML = arrayEmployee[0];
+                                        var cellPrenume = row.insertCell();
+                                        cellPrenume.innerHTML = arrayEmployee[1];
+                                        var cellEmail = row.insertCell();
+                                        cellEmail.innerHTML = arrayEmployee[2];
+                                        var cellSex = row.insertCell();
+                                        cellSex.innerHTML = arrayEmployee[3];
+                                        var cellDataNasterii = row.insertCell();
+                                        cellDataNasterii.innerHTML = arrayEmployee[4];
+                                        var cellPozaAngajat = row.insertCell();
+                                        cellPozaAngajat.innerHTML = `<img src="./images/${arrayEmployee[5]}" />`;
+                                        var cellDelete = row.insertCell();
+                                        cellDelete.innerHTML = '<img src="./images/recycle-bin.png" class="recycleBin"/>';
+                                        j++;
                                 }
+                            }else{
+                                alert("Introduceti o data de nastere valida");
+                            }
                         }
                     }
-                }
             }
         }
     }
