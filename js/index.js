3ed36f440fb44fe93b9363358d03f07f07619965
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-09-05 11:46:43 +0300

    Sorted by name and by birth date.

--- js/index.js
+++ js/index.js
@@ -42,7 +42,6 @@ function Add_Employee(event){
     var sex = document.getElementById("sex").value;
     var data_nasterii = document.getElementById("dataNasterii").value;    
     var poza_angajat = document.getElementById("poza").value;
-    var validari=0;
     if(nume==""){
         alert("Completati numele");
         event.preventDefault();
@@ -92,7 +91,7 @@ function Add_Employee(event){
                                     var table = document.getElementById("tabelAngajati");
                                         archive= localStorage.getItem(`${nume}`);
                                         var arrayEmployee =  archive.split(",");
-                                        var row= table.insertRow(localStorage.length+1);
+                                        var row= table.insertRow(localStorage.length);
                                         row.id = arrayEmployee[0]+"row";
                                         var cellNume = row.insertCell();
                                         cellNume.setAttribute("class", "td-paddings");
@@ -178,3 +177,82 @@ function deleteEmployee(emp,event){
     event.preventDefault();
 }
 
+function sort(listSelect, event){
+    if(listSelect.value=="nume"){
+        var archive = [];
+        var table = document.getElementById("tabelAngajati");
+        for (var i = 0; i < table.childNodes[1].childElementCount; i++) {
+            table.deleteRow(1);
+        }
+        var j=1;
+        for (var i = 0; i<localStorage.length; i++) {
+            archive[i] = localStorage.key(i);
+        }
+        var sortArray=archive.sort();
+    for (var i = 0; i<sortArray.length; i++) {
+        var archiveAll = [];
+        archiveAll[i] = localStorage.getItem(sortArray[i]);
+            var arrayEmployee =  archiveAll[i].split(",");
+            var row= table.insertRow(j);
+            row.id = arrayEmployee[0] +"row";
+            var cellNume = row.insertCell();
+            cellNume.setAttribute("class", "td-paddings");
+            cellNume.innerHTML = `<div id="icon-name"><img src="./images/${arrayEmployee[5]}"  id="imageEmployee"/><p id="nameEmployee"> ${arrayEmployee[0]}</p></div>`;
+            var cellPrenume = row.insertCell();
+            cellPrenume.setAttribute("class", "td-paddings");
+            cellPrenume.innerHTML = arrayEmployee[1];
+            var cellEmail = row.insertCell();
+            cellEmail.setAttribute("class", "td-paddings");
+            cellEmail.innerHTML = arrayEmployee[2];
+            var cellSex = row.insertCell();
+            cellSex.setAttribute("class", "td-paddings");
+            cellSex.innerHTML = arrayEmployee[3];
+            var cellDataNasterii = row.insertCell();
+            cellDataNasterii.setAttribute("class", "td-paddings");
+            cellDataNasterii.innerHTML = arrayEmployee[4];
+            var cellDelete = row.insertCell();
+            cellDelete.innerHTML = `<img src="./images/recycle-bin.png" class="recycleBin" onclick="return deleteEmployee(this,event)" id="${arrayEmployee[0]}"/>`;
+            j++;
+        }
+        event.preventDefault();
+    }else if(listSelect.value=="data-nasterii"){
+        var archive = [];
+        var table = document.getElementById("tabelAngajati");
+        for (var i = 0; i < table.childNodes[1].childElementCount; i++) {
+            table.deleteRow(1);
+        }
+        var j=1;
+        var archiveKeys =[];
+        for (var i = 0; i<localStorage.length; i++) {
+            archive[i] = localStorage.getItem(localStorage.key(i)).split(",")[4];
+            archiveKeys[i]=localStorage.getItem(localStorage.key(i)).split(",")[0];
+        }
+        var sortArray=archive.sort();
+    for (var i = 0; i<archiveKeys.length; i++) {
+        var archiveAll = [];
+        archiveAll[i] = localStorage.getItem(archiveKeys[i]);
+            var arrayEmployee =  archiveAll[i].split(",");
+            var row= table.insertRow(j);
+            row.id = arrayEmployee[0] +"row";
+            var cellNume = row.insertCell();
+            cellNume.setAttribute("class", "td-paddings");
+            cellNume.innerHTML = `<div id="icon-name"><img src="./images/${arrayEmployee[5]}"  id="imageEmployee"/><p id="nameEmployee"> ${arrayEmployee[0]}</p></div>`;
+            var cellPrenume = row.insertCell();
+            cellPrenume.setAttribute("class", "td-paddings");
+            cellPrenume.innerHTML = arrayEmployee[1];
+            var cellEmail = row.insertCell();
+            cellEmail.setAttribute("class", "td-paddings");
+            cellEmail.innerHTML = arrayEmployee[2];
+            var cellSex = row.insertCell();
+            cellSex.setAttribute("class", "td-paddings");
+            cellSex.innerHTML = arrayEmployee[3];
+            var cellDataNasterii = row.insertCell();
+            cellDataNasterii.setAttribute("class", "td-paddings");
+            cellDataNasterii.innerHTML = arrayEmployee[4];
+            var cellDelete = row.insertCell();
+            cellDelete.innerHTML = `<img src="./images/recycle-bin.png" class="recycleBin" onclick="return deleteEmployee(this,event)" id="${arrayEmployee[0]}"/>`;
+            j++;
+        }
+        event.preventDefault();
+    }
+}
